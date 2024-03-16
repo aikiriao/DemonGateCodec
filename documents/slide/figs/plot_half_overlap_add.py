320539511df3eb836d732c64a1dde73d6d06fe7a
aikiriao <samuraiaiki@gmail.com>
2024-03-16 09:40:35 +0900

    add half-verlap add.

---
+++ documents/slide/figs/plot_half_overlap_add.py
@@ -0,0 +1,79 @@
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+def mdct(indata):
+    N = len(indata) // 2
+    out = np.zeros(N)
+    for n in range(2 * N):
+        for k in range(N):
+            out[k] += indata[n] * np.cos(np.pi / N * ( k + 1/2 ) * ( n + 1/2 + N/2 ))
+    return out
+
+def imdct(inspec):
+    N = len(inspec)
+    out = np.zeros(2 * N)
+    for k in range(N):
+        for n in range(2 * N):
+            out[n] += inspec[k] * np.cos(np.pi / N * ( k + 1/2 ) * ( n + 1/2 + N/2 ))
+    out *= 2/N
+    return out
+
+if __name__ == "__main__":
+    LINES = ['-', '--', '-.', ':']
+    LINECYCLER = cycle(LINES)
+
+    FRAME_SIZE = 36
+    SLIDE_SIZE = 18
+    NUM_SAMPLES = FRAME_SIZE * 5
+
+    samples = np.arange(0, NUM_SAMPLES)
+    data = np.sin(2.0 * np.pi * samples / 30.0)
+
+    WINDOW = np.sin((np.arange(0, FRAME_SIZE) + 1/2) * np.pi / FRAME_SIZE)
+
+    smpl = 0
+    prev_frame = np.zeros(FRAME_SIZE // 2)
+    decoded_data = np.zeros_like(data)
+    while smpl + FRAME_SIZE <= NUM_SAMPLES:
+        inframe = data[smpl:smpl + FRAME_SIZE].copy()
+        inframe *= WINDOW
+        decframe = imdct(mdct(inframe)).copy()
+        decframe *= WINDOW
+        plt.cla()
+        plt.axhline(0.0, color='k')
+        plt.plot(inframe, label='input')
+        plt.axis('off')
+        plt.tight_layout()
+        plt.savefig(f'mdct_frame_waveform_{smpl}.png')
+        plt.cla()
+        plt.axhline(0.0, color='k')
+        plt.axvline(SLIDE_SIZE, color='k', linestyle='--')
+        plt.plot(decframe, label='IMDCT(MDCT(input))')
+        plt.axis('off')
+        plt.tight_layout()
+        plt.savefig(f'mdct_decoded_waveform_{smpl}.png')
+        decoded_data[smpl:smpl + SLIDE_SIZE] = prev_frame + decframe[:FRAME_SIZE // 2]
+        prev_frame = decframe[FRAME_SIZE // 2:]
+        smpl += SLIDE_SIZE
+
+    plt.cla()
+    plt.subplots(figsize=(12, 3))
+    plt.axhline(0.0, color='k')
+    for smpl in np.arange(0, NUM_SAMPLES - SLIDE_SIZE, SLIDE_SIZE):
+        plt.axvline(smpl, color='k', linestyle='--')
+    plt.plot(data[SLIDE_SIZE:-SLIDE_SIZE])
+    plt.axis('off')
+    plt.tight_layout()
+    plt.savefig(f'mdct_input_waveform.png')
+
+    plt.cla()
+    plt.subplots(figsize=(12, 3))
+    plt.axhline(0.0, color='k')
+    for smpl in np.arange(0, NUM_SAMPLES - SLIDE_SIZE, SLIDE_SIZE):
+        plt.axvline(smpl, color='k', linestyle='--')
+    plt.plot(decoded_data[SLIDE_SIZE:-SLIDE_SIZE])
+    plt.axis('off')
+    plt.tight_layout()
+    plt.savefig(f'mdct_decoded_waveform.png')
